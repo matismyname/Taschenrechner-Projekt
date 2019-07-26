9bc26f0fe945eeae4b6f0d6ac744acb9356b96d5
Metin <35172387+matismyname@users.noreply.github.com>
2019-07-26 13:52:47 +0200

    Update Ableitung.java

--- TaschenrechnerPlus/Ableitung.java
+++ TaschenrechnerPlus/Ableitung.java
@@ -1,21 +1,23 @@
 package TaschenrechnerPlus;
+
 import TaschenrechnerPlus.Runde;
 
-public class Ableitung{
+public class Ableitung {
 	double Abs[] = new double[8];
 	Runde r = new Runde();
+
 	public Ableitung(double arr[]) {
 		this.Abs = arr;
 	}
-    
-	//Einfache Summenregel für Ableitungen angewandt
-	//Das Array mit den abgeleiteten Koeffizienten wird zurückgegeben
-    public double[] ableite() {
-    	int n = 7;
-    	for(int i = 0; i<8; i++) {
-    		Abs[i] = r.runde((Abs[i] * n));
-    		n -= 1;
-    	}
+
+	// Einfache Summenregel für Ableitungen angewandt
+	// Das Array mit den abgeleiteten Koeffizienten wird zurückgegeben
+	public double[] ableite() {
+		int n = 7;
+		for (int i = 0; i < 8; i++) {
+			Abs[i] = r.runde((Abs[i] * n));
+			n -= 1;
+		}
 		return Abs;
-    }
+	}
 }
