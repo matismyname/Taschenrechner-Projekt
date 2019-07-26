c6eaf713e7248aef02066d3fa1fbe62de91051c5
Metin <35172387+matismyname@users.noreply.github.com>
2019-07-26 13:52:23 +0200

    Update package-info.java

--- Taschenrechner/package-info.java
+++ Taschenrechner/package-info.java
@@ -2,11 +2,13 @@
  * 
  */
 /**
- * @authors Benita Heid und Metin Bozkurt
- * Taschenrechner 2.0:
- * Dieser Taschenrechner kann Dezimal, Bruch und Komplex rechnen.
- * Ferner kann er Integrieren, Ableiten,
- * eine lineare Regressionsanalyse durchführen, Plotten, eine Zahl in seine
- * Primfaktoren zerlegen und Nullstellen bis zum zweiten Grad berechnen.
+ * @authors Benita Heid und Metin Bozkurt Taschenrechner 2.0: Dieser
+ *          Taschenrechner kann Dezimal, Bruch und Komplex rechnen. Ferner kann
+ *          er Integrieren, Ableiten, eine lineare Regressionsanalyse
+ *          durchführen, Plotten, von zwei Zahlen ihren GgT und ihren KgV
+ *          berechnen, und von einer Einheit in eine andere umrechnen und
+ *          Nullstellen bis zum zweiten Grad berechnen. Die Bedienung des
+ *          Taschenrechners ist in der ReadMe-Datei angegeben. Fertigstellung
+ *          des Projekts: 26.07.2019
  */
 package Taschenrechner;
