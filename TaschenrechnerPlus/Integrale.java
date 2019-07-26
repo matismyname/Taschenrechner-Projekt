769318cd6bd9bced70b453c40eb2c40e800531b5
Metin <35172387+matismyname@users.noreply.github.com>
2019-07-26 13:53:17 +0200

    Update Integrale.java

--- TaschenrechnerPlus/Integrale.java
+++ TaschenrechnerPlus/Integrale.java
@@ -1,21 +1,23 @@
 package TaschenrechnerPlus;
+
 import TaschenrechnerPlus.Runde;
 
-public class Integrale{
+public class Integrale {
 	double Ints[] = new double[8];
 	Runde r = new Runde();
+
 	public Integrale(double arr[]) {
 		this.Ints = arr;
 	}
-	
-	//Einfache Summenregel für Integrale angewandt
-	//Das Array mit den integrierten Koeffizienten wird zurückgegeben.
-    public double[] integriere() {
-    	int n = 8;
-    	for(int i = 0; i<8; i++) {
-    		Ints[i] = r.runde(Ints[i] * (1./n));
-    		n -= 1;
-    	}
+
+	// Einfache Summenregel für Integrale angewandt
+	// Das Array mit den integrierten Koeffizienten wird zurückgegeben.
+	public double[] integriere() {
+		int n = 8;
+		for (int i = 0; i < 8; i++) {
+			Ints[i] = r.runde(Ints[i] * (1. / n));
+			n -= 1;
+		}
 		return Ints;
-    }
+	}
 }
